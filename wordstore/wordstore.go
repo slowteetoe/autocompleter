c2afb2b7e8f0437281d599c0fed252d50825b66a
Steven Lotito <steven.lotito@alumni.pitt.edu>
2015-08-07 10:17:31 -0700

    Added /usr/share/dict/words to populate trie

--- wordstore/wordstore.go
+++ wordstore/wordstore.go
@@ -1,7 +1,12 @@
 package wordstore
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"slowteetoe.com/autocompleter/Godeps/_workspace/src/github.com/derekparker/trie"
+	"strings"
 )
 
 type Wordstore struct{}
@@ -13,12 +18,39 @@ func init() {
 	t.Add("cow", nil)
 	t.Add("coffee", nil)
 	t.Add("cake", nil)
+	PopulateDictionary(t)
 }
 
 func (w Wordstore) PrefixSearch(searchTerm string) []string {
 	results := []string{}
+
+	if searchTerm == "" {
+		log.Println("No search term provided.")
+		return results
+	}
 	if r := t.PrefixSearch(searchTerm); r != nil {
 		results = r
 	}
 	return results
+}
+
+func PopulateDictionary(t *trie.Trie) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	log.Println(pwd)
+	wordsfile,err := filepath.Abs("./data/words")
+	if err != nil {
+		panic(err)
+	}
+	b, err := ioutil.ReadFile(wordsfile)
+	if err != nil {
+		panic(err)
+	}
+	words := strings.Split(string(b[:]), "\n")
+
+	for _, element := range words {
+		t.Add(element, nil)
+	}
 }
